6690b6852c1728612f7242c00a8c90e6df50fc68
thanhtung1203 <1403hhtt@gmail.com>
2024-01-06 04:18:45 +0700

    bngu vcl

--- gallery/js.js
+++ gallery/js.js
@@ -6,3 +6,15 @@ document.getElementById('pre').onclick = function () {
     let list = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(list[list.length-1])
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var buyNowButtons = document.querySelectorAll(".buyNowButton");
+
+    buyNowButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var parentWrapper = this.closest('.wrapper');
+            var formWrapper = parentWrapper.querySelector('.buyNowFormWrapper');
+            formWrapper.classList.toggle("show-form");
+        });
+    });
+});
